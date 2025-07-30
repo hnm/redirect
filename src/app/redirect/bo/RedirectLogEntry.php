af42524282ea785f6bd6dd737d7dc87f1b87d0ba
Andreas von Burg <von-burg@hnm.ch>
2025-07-31 01:47:28 +0200

    php 8.4 compatibility

--- src/app/redirect/bo/RedirectLogEntry.php
+++ src/app/redirect/bo/RedirectLogEntry.php
@@ -5,6 +5,6 @@ namespace redirect\bo;
 use n2n\util\uri\Url;
 
 interface RedirectLogEntry {
-	public static function buildWithMessage(Url $url, RedirectRule $redirectRule, bool $success = true, string $errorMessage = null);
-	public static function createMessage(Url $url, RedirectRule $redirectRule, string $errorMessage = null);
+	public static function buildWithMessage(Url $url, RedirectRule $redirectRule, bool $success = true, ?string $errorMessage = null);
+	public static function createMessage(Url $url, RedirectRule $redirectRule, ?string $errorMessage = null);
 }
